5a03b1877130b49cdfcf6aca1c9a08f32330064b
kento <kento@toriwatari.bird>
2021-10-20 21:06:51 +0900

    🏃‍♂️ Update index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
 import * as React from "react"
 import { Link, graphql } from "gatsby"
 
+/*
 import Layout from "../components/layout"
 import Seo from "../components/seo"
+*/
 
 const Styles = require("../styles/index.module.scss")
 
@@ -16,16 +18,16 @@ config.autoAddCss = false
 const Index: React.VFC = () => (
     <>
       <header className={Styles.header} id="header">
-        <h2 className="header-sub-title">toriwatari</h2>
-        <h1 className="header-title">Portfolio Web Site</h1>
-        <p>更新日 : <time>2021-10-17</time></p>
+        <h2 className={Styles.headerSubTitle}>toriwatari</h2>
+        <h1 className={Styles.headerTitle}>Portfolio Website</h1>
+        <p>更新日 : <time>2021-10-20</time></p>
 
-        <FontAwesomeIcon icon={faHeart} />
+        <h3 className={Styles.message}>I <FontAwesomeIcon icon={faHeart} /> HTML & CSS</h3>
 
-        <a className="arrow-container" href="#sec1">
-          <div className="arrow"></div>
-          <div className="arrow"></div>
-          <div className="arrow"></div>
+        <a className={Styles.arrowContainer} href="#sec1">
+          <div className={Styles.arrow}></div>
+          <div className={Styles.arrow}></div>
+          <div className={Styles.arrow}></div>
         </a>
       </header> 
     </>
