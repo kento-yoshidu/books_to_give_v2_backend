065e3a7d35ab85c1e8ee3fc6421cabbc0e5ce0d6
kento <kento@toriwatari.bird>
2021-11-01 19:38:25 +0900

    🏃‍♂️ スクロール追従を実装

--- src/pages/Jamstack/index.tsx
+++ src/pages/Jamstack/index.tsx
@@ -13,11 +13,49 @@ import {
 //import IO from "./intersectionObserver"
 
 const Jamstack = () => {
-  /*
+  const InterSectionObserver = () => {
+    const boxes = document.querySelectorAll(`.${Styles.view}`);
+    console.log(boxes)
+
+    const options = {
+      root: null,
+      rootMargin: "-50% 0px",
+      threshold: 0
+    }
+
+    const observer = new IntersectionObserver(doWhenIntersect, options);
+
+    boxes.forEach(box => {
+      observer.observe(box)
+    })
+
+    function doWhenIntersect(entries) {
+      entries.forEach(entry => {
+        if (entry.isIntersecting) {
+          activateIndex(entry.target)
+        }
+      })
+    }
+
+    function activateIndex(element) {
+      // すでにアクティブになっている目次を選択
+      const currentActiveIndex = document.querySelector(`#viewList .${Styles.active}`);
+      console.log(currentActiveIndex)
+
+      // すでにアクティブになっているものが0個の時（=null）以外は、activeクラスを除去
+      if (currentActiveIndex !== null) {
+        currentActiveIndex.classList.remove(Styles.active);
+      }
+      // 引数で渡されたDOMが飛び先のaタグを選択し、activeクラスを付与
+      //const newActiveIndex = document.querySelector(`a[href='#${element.id}']`);
+      const newActiveIndex = document.querySelector(`li[data-li='${element.id}']`);
+      newActiveIndex.classList.add(Styles.active);
+    }
+  }
+
   useEffect(() => {
-    IO();
+    InterSectionObserver()
   }, [])
-  */
 
   return (
     <div className={Styles.allWrapper}>
@@ -206,7 +244,7 @@ const Jamstack = () => {
     </div>
 	</section>
 
-  <section className="view view4" id="view4">
+  <section className={`${Styles.section} ${Styles.view} ${Styles.view4}`} id="view4">
     <br />
   </section>
     </div>
