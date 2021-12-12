aaefb0fcf27e07b20964d0451b9e4bb61aa7f3b4
kento <kento@toriwatari.bird>
2021-12-12 20:32:20 +0900

    ✨ Update Sample4

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -93,24 +93,6 @@ const Index: React.VFC = () => {
           <FontAwesomeIcon icon={faCode} />
           <p>サンプルページ4</p>
         </Link>
-
-        <Link
-          className={Styles.linkItem}
-          to="sample-page/sample5"
-        >
-          <h3 className={`${Styles.title} ${Styles.linkTitle}`}>Sample5</h3>
-          <FontAwesomeIcon icon={faCode} />
-          <p>サンプルページ5</p>
-        </Link>
-
-        <Link
-          className={Styles.linkItem}
-          to="sample-page/sample6"
-        >
-          <h3 className={`${Styles.title} ${Styles.linkTitle}`}>Sample6</h3>
-          <FontAwesomeIcon icon={faCode} />
-          <p>サンプルページ6</p>
-        </Link>
       </div>
 
       <div className={`${Styles.sa} ${Styles.saUp}`}>
