27a7c75a060266faec2ca73963e8e1a66a78d802
kento <kento@toriwatari.bird>
2022-02-27 20:50:46 +0900

    ✨ Update Sample3

--- src/pages/sample-page/sample3/index.tsx
+++ src/pages/sample-page/sample3/index.tsx
@@ -4,97 +4,29 @@ import Particles from "react-tsparticles"
 
 import * as Styles from "./scss/style.module.scss"
 
-const options = {
-  fpsLimit: 60,
-  interactivity: {
-    events: {
-      onClick: {
-        enable: false
-      },
-      onHover: {
-        enable: false,
-        mode: "repulse"
-      },
-      resize: true
-    },
-    modes: {
-      bubble: {
-        distance: 400,
-        duration: 2,
-        opacity: 0.8,
-        size: 40
-      },
-      push: {
-        quantity: 4
-      },
-      repulse: {
-        distance: 200,
-        duration: 0.4
-      }
-    }
-  },
-  particles: {
-    color: {
-      value: "#ffffff"
-    },
-    links: {
-      color: "#ffffff",
-      distance: 150,
-      enable: true,
-      opacity: 0.5,
-      width: 1
-    },
-    collisions: {
-      enable: true
-    },
-    move: {
-      direction: "top-right",
-      enable: true,
-      outMode: "out",
-      random: false,
-      speed: 1,
-      straight: false
-    },
-    number: {
-      density: {
-        enable: true,
-        value_area: 800
-      },
-      value: 75
-    },
-    opacity: {
-      value: 0.5
-    },
-    shape: {
-      type: "circle"
-    },
-    size: {
-      random: true,
-      value: 5
-    }
-  },
-  detectRetina: true
-}
+import ParticlesConfig from "../../../lib/Sample3/particles"
 
 const Sample3 = () => (
   <>
     <header className={Styles.header} id="js-header">
       <Particles
         id="tsparticles"
-        options={options}
+        options={ParticlesConfig}
       />
       <div className={`${Styles.circle} ${Styles.circle1}`}></div>
       <div className={`${Styles.circle} ${Styles.circle2}`}></div>
       <div className={`${Styles.circle} ${Styles.circle3}`}></div>
 
-      <div className={Styles.textWrapper}>
+      <div className={Styles.headerTextWrapper}>
         <h1 className={Styles.headerTitle}>
           Sample Page 3
         </h1>
+
         <h2 className={Styles.productTitle}>
           Sample Web Service
         </h2>
-        <p className={Styles.text}>
+
+        <p className={Styles.headerText}>
           <span>静</span>
           <span>的</span>
           <span>ホ</span>
@@ -104,7 +36,8 @@ const Sample3 = () => (
           <span>ン</span>
           <span>グ</span>
         </p>
-        <p className={Styles.text}>
+
+        <p className={Styles.headerText}>
           <span>自</span>
           <span>動</span>
           <span>デ</span>
@@ -112,7 +45,8 @@ const Sample3 = () => (
           <span>ロ</span>
           <span>イ</span>
         </p>
-        <p className={Styles.text}>
+
+        <p className={Styles.headerText}>
           <span>ロ</span>
           <span>ー</span>
           <span>ド</span>
@@ -123,7 +57,8 @@ const Sample3 = () => (
           <span>ン</span>
           <span>グ</span>
         </p>
-        <p className={Styles.text}>
+
+        <p className={Styles.headerText}>
           <span>マ</span>
           <span>イ</span>
           <span>ク</span>
@@ -141,62 +76,76 @@ const Sample3 = () => (
         <h2 className={Styles.sectionTitle}>Contents</h2>
 
         <section className={Styles.contents1}>
-          <div className={Styles.sectionWrapper}>
-            <div className={Styles.introduction}>
-              <div className={Styles.numbering}>01</div>
+          <div className={Styles.contentsSectionWrapper}>
+            <div className={Styles.contentsIntroduction}>
+              <div className={Styles.contentsNumbering}>01</div>
               <h2 className={`${Styles.title} ${Styles.contentsTitle}`}>CSS Animation & Tips</h2>
-              <p className={Styles.text}>CSSのみで実現できるアニメーションや、様々なTipsを紹介します。Nuxt.jsで構築しています。</p>
+              <p className={Styles.contentsText}>CSSのみで実現できるアニメーションや、様々なTipsを紹介します。Gatsbyで構築しています。</p>
             </div>
 
             <StaticImage
               src="./svg/section1-bg.svg"
               alt="hoge"
-              className={Styles.sectionImage}
-              // imgClassName={Styles.svgImage}
+              className={Styles.contentsImage}
             />
           </div>
 
           <div className={Styles.btnWrapper}>
-            <a href="https://cssanimation-and-tips.netlify.app/" className="link-btn" target="_blank">詳しく見る <i className="fas fa-external-link-alt"></i></a>
+            <a
+              href="https://cssanimation-and-tips.netlify.app/"
+              className={Styles.btn}
+              target="_blank"
+              rel="noreferrer"
+            >
+              詳しく見る
+              <i className="fas fa-external-link-alt"></i>
+            </a>
           </div>
         </section>
 
         <section className={Styles.contents2}>
-          <div className={Styles.sectionWrapper}>
+          <div className={Styles.contentsSectionWrapper}>
             <StaticImage
               src="./svg/section2-bg.svg"
               alt="hoge"
-              className={Styles.sectionImage}
-              imgClassName={Styles.svgImage}
+              className={Styles.contentsImage}
             />
 
-            <div className={Styles.introduction}>
-              <div className={Styles.numbering}>02</div>
+            <div className={Styles.contentsIntroduction}>
+              <div className={Styles.contentsNumbering}>02</div>
               <h2 className={`${Styles.title} ${Styles.contentsTitle}`}>Blog</h2>
-              <p className={Styles.text}>技術・日記をつづるためのブログ「鳥に生まれることができなかった人へ」です。</p>
-              <p className={Styles.text}>静的サイトジェネレーターのGatsbyを使用しています。AWSでホスティングしています。</p>
-            </div>
-          </div>
-
-          <div className={Styles.btnWrapper}>
-            <a href="https://blog.toriwatari.work/page/1/" className="link-btn" target="_blank">詳しく見る <i className="fas fa-external-link-alt"></i></a>
-          </div>
-        </section>
-      </section>
-
-      <section className={Styles.sec3}>
-        <div className={Styles.sectionWrapper}>
-          <div className={Styles.introduction}>
-            <div className={Styles.inner}>
-              <h2 className={`${Styles.title} ${Styles.sectionTitle}`}>JamstackなWebサイトを構築してみた</h2>
-              <p>Jamstack構成のWebサイト構築が面白いと感じ、サンプルで色々なサイトを作成しています。</p>
-              <p>2021年度中に10個作成するのが目標です。</p>
+              <p className={Styles.contentsText}>技術・日記をつづるためのブログ「鳥に生まれることができなかった人へ」です。</p>
+              <p className={Styles.contentsText}>静的サイトジェネレーターのGatsbyを使用しています。AWSでホスティングしています。</p>
             </div>
           </div>
 
           <div className={Styles.btnWrapper}>
             <a
+              href="https://blog.toriwatari.work/page/1/"
+              className={Styles.btn}
+              target="_blank"
+              rel="noreferrer"
+            >
+              詳しく見る <i className="fas fa-external-link-alt"></i>
+            </a>
+          </div>
+        </section>
+      </section>
+
+      <section className={Styles.sec3}>
+        <div className={Styles.sec3SectionWrapper}>
+          <div className={Styles.sec3Introduction}>
+            <div className={Styles.sec3Inner}>
+              <h2 className={`${Styles.title} ${Styles.sec3SectionTitle}`}>JamstackなWebサイトを構築してみた</h2>
+              <p>Jamstack構成のWebサイト構築が面白いと感じ、サンプルで色々なサイトを作成しています。</p>
+              <p>2021年度中に10個作成するのが目標です。</p>
+            </div>
+          </div>
+
+          <div className={`${Styles.btnWrapper} ${Styles.sec3BtnWrapper}`}>
+            <a
               href="https://ps.toriwatari.work/Jamstack/"
+              className={Styles.btn}
             >
               詳しく見る
             </a>
@@ -208,39 +157,39 @@ const Sample3 = () => (
         <h2 className={Styles.sectionTitle}>導入事例 - お客様からの声 - </h2>
 
         <div className={`${Styles.board} ${Styles.board1}`}>
-          <div className={Styles.index}>01</div>
-          <h3 className={Styles.title}>幽霊が出るお店として恐れられていたのに…</h3>
+          <div className={Styles.boardIndex}>01</div>
+          <h3 className={Styles.boardTitle}>幽霊が出るお店として恐れられていたのに…</h3>
           <StaticImage
             src="./svg/sec4-1.svg"
-            className={Styles.iconWrapper}
+            className={Styles.boardIconWrapper}
             alt="hoge"
           />
-          <p className={Styles.text}>AI技術で幽霊を検知し、その怨念をクラウドストレージ上に逃がすシステムを導入していただきました。</p>
-          <p className={Styles.text}>おかげさまで逃げていた客足も戻り、大変忙しくされていただいております。</p>
-          <p className={Styles.text}>インスタンスの自動スケーリングにも対応しており、突発的な幽霊増にも安心です。</p>
-          <p className={Styles.client}>株式会社 黒毛牧場</p>
+          <p className={Styles.boardText}>AI技術で幽霊を検知し、その怨念をクラウドストレージ上に逃がすシステムを導入していただきました。</p>
+          <p className={Styles.boardText}>おかげさまで逃げていた客足も戻り、大変忙しくされていただいております。</p>
+          <p className={Styles.boardText}>インスタンスの自動スケーリングにも対応しており、突発的な幽霊増にも安心です。</p>
+          <p className={Styles.boardClient}>株式会社 黒毛牧場</p>
         </div>
 
         <div className={`${Styles.board} ${Styles.board2}`}>
-          <div className={Styles.index}>02</div>
-          <h3 className={Styles.title}>精度の高い嘘判定機</h3>
+          <div className={Styles.boardIndex}>02</div>
+          <h3 className={Styles.boardTitle}>精度の高い嘘判定機</h3>
           <StaticImage
             src="./svg/sec4-2.svg"
-            className={Styles.iconWrapper}
+            className={Styles.boardIconWrapper}
             alt="hoge"
           />
-          <p className={Styles.text}>前評判通り、ユーザ登録から5分も立たずにサービスを利用できることに驚きました。</p>
-          <p className={Styles.text}>とある新興宗教の教祖をやっているんですが、信者たちがどれほどの忠誠心を持っているかの判定にこの嘘判定システムはぴったりです。</p>
-          <p className={Styles.text}>無料プランでここまでできるんだから驚きです。</p>
-          <p className={Styles.client}>宗教法人 ハッピーホームラン代表</p>
+          <p className={Styles.boardText}>前評判通り、ユーザ登録から5分も立たずにサービスを利用できることに驚きました。</p>
+          <p className={Styles.boardText}>とある新興宗教の教祖をやっているんですが、信者たちがどれほどの忠誠心を持っているかの判定にこの嘘判定システムはぴったりです。</p>
+          <p className={Styles.boardText}>無料プランでここまでできるんだから驚きです。</p>
+          <p className={Styles.boardClient}>宗教法人 ハッピーホームラン代表</p>
         </div>
 
         <div className={`${Styles.board} ${Styles.board3}`}>
-          <div className={Styles.index}>03</div>
-          <h3 className={Styles.title}>ဒီ site သည်အယောင်ဆောင်ပြီး</h3>
+          <div className={Styles.boardIndex}>03</div>
+          <h3 className={Styles.boardTitle}>ဒီ site သည်အယောင်ဆောင်ပြီး</h3>
           <StaticImage
             src="./svg/sec4-3.svg"
-            className={Styles.iconWrapper}
+            className={Styles.boardIconWrapper}
             alt="hoge"
           />
           <p className={Styles.text}>ထိုကဲ့သို့ site ောဆိုဒ်များနှင့်ထုတ်ကုန်မတည်ရှိပါဘူး။</p>
