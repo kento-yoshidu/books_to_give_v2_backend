0eb75faf6475759aaef2e0c255b35f80e8a96d83
kento <kento@toriwatari.bird>
2022-05-02 19:58:14 +0900

    ✨ Update

---
+++ src/pages/sample-page/sample6/index.tsx
@@ -0,0 +1,19 @@
+import React from "react"
+
+import Seo from "../../../components/seo"
+
+import * as Styles from "./scss/style.module.scss"
+
+const Sample6 = () => (
+  <>
+    <Seo
+      title="Sample5"
+    />
+
+    <div className={Styles.body}>
+      <h1>Sample6</h1>
+    </div>
+  </>
+)
+
+export default Sample6
