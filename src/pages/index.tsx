2fb1732263ad311f8308f7f6427ca6116d8c3af2
kento <kento@toriwatari.bird>
2022-06-11 16:31:54 +0900

    ✨ Update

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -26,19 +26,18 @@ const Index: React.VFC = () => {
 
   return (
     <>
-      <Seo
-        title="ポートフォリオサイト"
-      />
+      <Seo title="ポートフォリオサイト" />
 
       <div className={Styles.sa}>
         <Header />
       </div>
 
       <section className={`${Styles.section} ${Styles.sec1}`} id="sec1">
-        <h2 className={`${Styles.sectionTitle} ${Styles.linksTitle}`}>Sample Pages</h2>
+        <h2 className={`${Styles.sectionTitle} ${Styles.linksTitle}`}>
+          Sample Pages
+        </h2>
 
         <div className={`${Styles.linksWrapper} ${Styles.sa} ${Styles.saUp}`}>
-
           <SamplePage
             path="sample1"
             title="Sample1"
@@ -86,13 +85,13 @@ const Index: React.VFC = () => {
           />
         </div>
 
-        <Button
-          path="sample-page"
-          text="more"
-        />
+        <Button path="sample-page" text="more" />
       </section>
 
-      <section className={`${Styles.section} ${Styles.sa} ${Styles.saUp}`} id="sec3">
+      <section
+        className={`${Styles.section} ${Styles.sa} ${Styles.saUp}`}
+        id="sec3"
+      >
         <h2 className={Styles.sectionTitle}>CSS Animation & Tips</h2>
 
         <div className={Styles.sectionWrapper}>
@@ -102,19 +101,25 @@ const Index: React.VFC = () => {
             layout="fullWidth"
             className={Styles.imgWrapper}
           />
-          <p>CSSで実現できるアニメーションや、ちょっとした小テクを紹介します。Gatsby + MDXファイル + Netlifyで構築しました。</p>
+          <p>
+            CSSで実現できるアニメーションや、ちょっとした小テクを紹介します。Gatsby
+            + MDXファイル + Netlifyで構築しました。
+          </p>
         </div>
-        <Button
-          path="https://cssanimation-and-tips.netlify.app"
-          text="more"
-        />
+        <Button path="https://cssanimation-and-tips.netlify.app" text="more" />
       </section>
 
-      <section className={`${Styles.section} ${Styles.sa} ${Styles.saUp}`} id="sec4">
+      <section
+        className={`${Styles.section} ${Styles.sa} ${Styles.saUp}`}
+        id="sec4"
+      >
         <h2 className={Styles.sectionTitle}>Blog</h2>
 
         <div className={Styles.sectionWrapper}>
-          <p>技術ネタや日記のためのブログ「鳥に生まれることができなかった人へ」を作りました。Gatsby + MDファイル + AWS Amplifyで構築しています。</p>
+          <p>
+            技術ネタや日記のためのブログ「鳥に生まれることができなかった人へ」を作りました。Gatsby
+            + MDファイル + AWS Amplifyで構築しています。
+          </p>
           <StaticImage
             src="../images/gatsby.png"
             alt="CSSのイメージ画像"
@@ -123,16 +128,20 @@ const Index: React.VFC = () => {
           />
         </div>
 
-        <Button
-          path="https://blog.toriwatari.work"
-          text="more"
-        />
+        <Button path="https://blog.toriwatari.work" text="more" />
       </section>
 
-      <section className={`${Styles.section} ${Styles.sa} ${Styles.saUp}`} id="sec5">
-        <h2 className={Styles.sectionTitle}>JamstackなWebサイトを構築してみた</h2>
+      <section
+        className={`${Styles.section} ${Styles.sa} ${Styles.saUp}`}
+        id="sec5"
+      >
+        <h2 className={Styles.sectionTitle}>
+          JamstackなWebサイトを構築してみた
+        </h2>
         <div className={Styles.sectionWrapper}>
-          <p>Jamstack構成のWebサイト構築が面白いと感じ、サンプルで色々なサイトを作成しています。2021年度中に10個作成するのが目標です。</p>
+          <p>
+            Jamstack構成のWebサイト構築が面白いと感じ、サンプルで色々なサイトを作成しています。2021年度中に10個作成するのが目標です。
+          </p>
           <StaticImage
             src="../images/static.png"
             alt="CSSのイメージ画像"
@@ -141,109 +150,31 @@ const Index: React.VFC = () => {
           />
         </div>
 
-        <Button
-          path="Jamstack"
-          text="more"
-        />
+        <Button path="Jamstack" text="more" />
       </section>
 
-      <section className={`${Styles.section} ${Styles.sec5} progress-section sa sa--up`}>
+      <section
+        className={`${Styles.section} ${Styles.sec5} progress-section sa sa--up`}
+      >
         <h2 className={Styles.sectionTitle}>2022年度の個人タスク</h2>
 
-        <div style={{ "display": "flex", "justifyContent": "center", "gap": "30px", "flexWrap": "wrap", "width": "80%", "margin": "0 auto" }}>
-          <Card
-            color="#04fc43"
-            num="85"
-            text="Jamstackサイトを10個作る"
-          />
+        <div
+          style={{
+            display: "flex",
+            justifyContent: "center",
+            gap: "30px",
+            flexWrap: "wrap",
+            width: "80%",
+            margin: "0 auto",
+          }}
+        >
+          <Card color="#04fc43" num="50" text="Jamstackサイトを10個作る" />
 
-          <Card
-            color="#06ccff"
-            num="70"
-          />
+          <Card color="#06ccff" num="50" text="AWS認定資格を2つ以上取る" />
 
-          <Card
-            color="#ff00be"
-            num="50"
-          />
+          <Card color="#ff00be" num="45" text="サンプルWebサイトを10個作る" />
 
-          <Card
-            color="#fee800"
-            num="10"
-          />
-        </div>
-        <div className={Styles.wrapper}>
-          <div className={Styles.running}>
-            <h3>Running...<span className={Styles.emoji}>️🏃️💨</span></h3>
-
-            <div className={Styles.progressbarWrapper}>
-              <h4>Jamstackなサイトを10個作る</h4>
-              <span>50%</span>
-              <div className={Styles.progressbar}>
-                <div className={Styles.side}>
-                  <span style={{ "--i": "50%" }}></span>
-                </div>
-              </div>
-            </div>
-
-            <div className={Styles.progressbarWrapper}>
-              <h4>AWS認定資格を2つ以上取る</h4>
-              <span>50%</span>
-              <div className={Styles.progressbar}>
-                <div className={Styles.side}>
-                  <span style={{ "--i": "50%" }}></span>
-                </div>
-              </div>
-            </div>
-
-            <div className={Styles.progressbarWrapper}>
-              <h4>サンプルWebサイトを20個作る</h4>
-              <span>45%</span>
-              <div className={Styles.progressbar}>
-                <div className={Styles.side}>
-                  <span style={{ "--i": "45%" }}></span>
-                </div>
-              </div>
-            </div>
-
-            <div className={Styles.progressbarWrapper}>
-              <h4>最新のCSSを学習する</h4>
-              <span>20%</span>
-              <div className={Styles.progressbar}>
-                <div className={Styles.side}>
-                  <span style={{ "--i": "20%" }}></span>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          {/*
-          <div className={Styles.hurryUp}>
-            <h3>Hurry Up‍<span className={Styles.emoji}>️🏇️💦</span></h3>
-
-            <div className={Styles.progressbarWrapper}>
-              <h4>ECMAScript2021を全部読む</h4>
-              <span>1%</span>
-              <div className={Styles.progressbar}>
-                <div className={Styles.side}>
-                  <div className={`${Styles.bar} ${Styles.barEcma}`}></div>
-                </div>
-              </div>
-            </div>
-          </div>
-
-          <div className={Styles.finished}>
-            <h3><span className={Styles.emoji}>️🚀</span> Finished&nbsp;‍<span className="emoji">️🚀</span></h3>
-
-            <ul className={Styles.finishedList}>
-              <div className={Styles.progressbarWrapper}>
-                <h4>🎊中小企業診断士 1次試験合格🎊</h4>
-                <br />
-                <h4>🎊GatsbyBlogの完成度を上げる🎊</h4>
-              </div>
-            </ul>
-          </div>
-          */}
+          <Card color="#fee800" num="20" text="最新のCSSを学習する" />
         </div>
       </section>
 
