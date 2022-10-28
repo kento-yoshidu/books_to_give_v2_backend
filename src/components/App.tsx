d013c64de8db2718003cc4c4df1f15a52c3ccb16
kento <kento@toriwatari.bird>
2022-10-28 13:38:28 +0900

    ✨ Appコンポーネントの更新

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,23 +1,26 @@
 import React from "react"
 
+import * as Styles from "../styles/App.module.scss"
+
 type Props = {
   title: string
   url: string
   description: string
   technologies: string[]
+  isOpen?: boolean
 }
 
-const App = ({ title, url, description, technologies }: Props) => {
+const App = ({ title, url, description, technologies, isOpen }: Props) => {
   return (
-    <details>
-      <summary>
-        <a href={url}>{title}</a>
+    <details className={Styles.details}>
+      <summary className={Styles.summary}>
+        <a href={url} className={Styles.title}>{title}</a>
         <p>{description}</p>
       </summary>
 
-      <ul>
+      <ul className={Styles.technologies}>
         {technologies.map((tech) => (
-          <li key={`tech${tech}`}>{tech}</li>
+          <li key={`tech${tech}`}>⚙ {tech}</li>
         ))}
       </ul>
     </details>
