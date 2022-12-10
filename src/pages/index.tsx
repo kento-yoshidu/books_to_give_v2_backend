033731c6739b2d5ba285e78f63d66375686d7162
kento <kento@toriwatari.bird>
2022-12-10 16:00:45 +0900

    ✨ 不要なファイルを削除

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -6,9 +6,7 @@ import Button from "../components/button"
 import intersectionObserver from "../lib/intersectionObserver"
 
 import Header from "../components/header"
-import SamplePage from "../components/samplePage"
 import Card from "../components/card"
-import App from "../components/App"
 
 import * as Styles from "../styles/index.module.scss"
 
@@ -30,131 +28,6 @@ const Index: React.VFC = () => {
           Sample Pages
         </h2>
 
-        <div className={`${Styles.linksWrapper} ${Styles.sa} ${Styles.saUp}`}>
-          <SamplePage
-            path="https://toriwatari-sample1.netlify.app/"
-            title="Sample1"
-            text="サンプルページ1です。"
-          />
-
-          <SamplePage
-            path="sample2"
-            title="Sample2"
-            text="サンプルページ2です。"
-          />
-
-          <SamplePage
-            path="sample3"
-            title="Sample3"
-            text="サンプルページ3です。"
-          />
-
-          <SamplePage
-            path="sample4"
-            title="Sample4"
-            text="サンプルページ4です。"
-          />
-
-          <SamplePage
-            path="sample5"
-            title="Sample5"
-            text="サンプルページ5です。"
-          />
-        </div>
-
-        <Button path="sample-page" text="more" />
-      </section>
-
-      <section className={`${Styles.section} ${Styles.sec1}`} id="sec1">
-        <h2 className={`${Styles.sectionTitle} ${Styles.linksTitle}`}>
-          Apps
-        </h2>
-
-        <App
-          title="鳥に生まれることが出来なかった人へ"
-          url="https://bookstogive-kento-yoshidu.vercel.app/f"
-          description="Gatsbyで作ったブログ。"
-          technologies={["Gatsby", "TypeScript", "CSS Modules", "AWS Amplify"]}
-        />
-
-        <App
-          title="BooksToGive"
-          url="https://bookstogive-kento-yoshidu.vercel.app/f"
-          description="読み終わって誰かにあげたい本をリストアップ。"
-          technologies={["Next.js", "TypeScript", "Tailwind CSS", "Supabase", "Prisma", "Vercel", "Google Books API"]} 
-        />
-
-        <App
-          title="CSS Animation & Tips"
-          url="#"
-          description="改装中。。。"
-          technologies={["Gatsby", "TypeScript", "CSS Modules"]}
-        />
-      </section>
-
-      <section
-        className={`${Styles.section} ${Styles.sa} ${Styles.saUp}`}
-        id="sec3"
-      >
-        <h2 className={Styles.sectionTitle}>CSS Animation & Tips</h2>
-
-        <div className={Styles.sectionWrapper}>
-          <StaticImage
-            src="../images/css.png"
-            alt="CSSのイメージ画像"
-            layout="fullWidth"
-            className={Styles.imgWrapper}
-          />
-          <p>
-            CSSで実現できるアニメーションや、ちょっとした小テクを紹介します。Gatsby
-            + MDXファイル + Netlifyで構築しました。
-          </p>
-        </div>
-        <Button path="https://cssanimation-and-tips.netlify.app" text="more" />
-      </section>
-
-      <section
-        className={`${Styles.section} ${Styles.sa} ${Styles.saUp}`}
-        id="sec4"
-      >
-        <h2 className={Styles.sectionTitle}>Blog</h2>
-
-        <div className={Styles.sectionWrapper}>
-          <p>
-            技術ネタや日記のためのブログ「鳥に生まれることができなかった人へ」を作りました。Gatsby
-            + MDファイル + AWS Amplifyで構築しています。
-          </p>
-          <StaticImage
-            src="../images/gatsby.png"
-            alt="CSSのイメージ画像"
-            layout="fullWidth"
-            className={Styles.imgWrapper}
-          />
-        </div>
-
-        <Button path="https://blog.toriwatari.work" text="more" />
-      </section>
-
-      <section
-        className={`${Styles.section} ${Styles.sa} ${Styles.saUp}`}
-        id="sec5"
-      >
-        <h2 className={Styles.sectionTitle}>
-          JamstackなWebサイトを構築してみた
-        </h2>
-        <div className={Styles.sectionWrapper}>
-          <p>
-            Jamstack構成のWebサイト構築が面白いと感じ、サンプルで色々なサイトを作成しています。2021年度中に10個作成するのが目標です。
-          </p>
-          <StaticImage
-            src="../images/static.png"
-            alt="CSSのイメージ画像"
-            layout="fullWidth"
-            className={Styles.imgWrapper}
-          />
-        </div>
-
-        <Button path="Jamstack" text="more" />
       </section>
 
       <section
