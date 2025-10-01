bc9affae16e20cede138ba15ca30a2c08cd18ba3
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-10-01 20:48:41 +0900

    ⚡ Install Actix

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
-fn main() {
-    println!("Hello, world!");
+use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+
+#[get("/hello")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello, world!")
+}
+
+#[actix_web::main]
+async fn main() -> std::io::Result<()> {
+    HttpServer::new(|| {
+        App::new()
+            .service(hello)
+    })
+    .bind(("0.0.0.0", 8000))?
+    .run()
+    .await
 }
