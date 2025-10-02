b5e2b592989e856430096f94013e8e533833e8ce
kento-yoshidu <kento_yoshidu@yahoo.co.jp>
2025-10-02 21:09:45 +0900

    ⚡ Update

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,22 @@
-use actix_web::{get, App, HttpServer, Responder, HttpResponse};
+use std::fs;
+use actix_web::{get, web, App, HttpServer, Responder, HttpResponse};
+use serde::{Deserialize, Serialize};
+
+#[derive(Debug, Serialize, Deserialize)]
+struct Book {
+    id: u32,
+    title: String,
+    author: String,
+}
+
+#[get("/books")]
+async fn get_books() -> impl Responder {
+    let data = fs::read_to_string("data.json").expect("json error");
+
+    let books: Vec<Book> = serde_json::from_str(&data).expect("parse error");
+
+    web::Json(books)
+}
 
 #[get("/hello")]
 async fn hello() -> impl Responder {
@@ -10,6 +28,7 @@ async fn main() -> std::io::Result<()> {
     HttpServer::new(|| {
         App::new()
             .service(hello)
+            .service(get_books)
     })
     .bind(("0.0.0.0", 8000))?
     .run()
